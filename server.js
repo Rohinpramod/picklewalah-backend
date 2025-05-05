d06980cf212e3ed497b6cffe5e234011748cd62a
ROHIN PRAMOD <rohinpramod320@gmail.com>
2025-05-05 13:48:34 +0530

    admin orgin

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser()); 
 app.use(cors({
-    origin:['https://picklewalah.com','pickle-walah-dashboard.vercel.app'],
+    origin:['https://picklewalah.com','https://pickle-walah-dashboard.vercel.app'],
     credentials:true,
     methods: ["GET","POST","PUT","PATCH","DELETE"],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
