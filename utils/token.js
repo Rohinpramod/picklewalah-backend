bba61c511636b2b46f62ad461e9b9e8de034a181
ROHIN PRAMOD <rohinpramod320@gmail.com>
2025-05-05 17:02:04 +0530

    token expire

--- utils/token.js
+++ utils/token.js
@@ -1,10 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-exports.generateToken = (user,role)=>{
-    try{
-        var token = jwt.sign({id:user._id, role:user.role},process.env.JWT_SECRET);
-        return token
-    }catch(error){
-        console.log(error)
+exports.generateToken = (user, role) => {
+    try {
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: '2h' }  // Token expires in 2 hours
+        );
+        return token;
+    } catch (error) {
+        console.log(error);
     }
-}
+};
